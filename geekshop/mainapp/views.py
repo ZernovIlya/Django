e8a08db69193ce7e01c42b366d1c91a201cf0c4c
zernov <zernov.92@gmail.com>
2019-05-17 02:42:43 +0300

    hw less5

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, ProductCategory
+from basketapp.models import BasketSlot
 
 def main(request):
-    context = {'user':{'name':'человек'}}
-    return render(request, 'mainapp/index.html', context)
+    context = {'user': {'name': 'человек'}}
+    return render(request, 'mainapp/index.html')
+
 
 def products(request, pk=None):
-    # Product.objects.filter(category=pk)
-    context = {'products': Product.objects.all()}
+    basket = []
+    if request.user.is_authenticated:
+        basket = BasketSlot.objects.filter(user=request.user)
+
+    total_quantity = sum(list(map(lambda basket_slot: basket_slot.quantity, basket)))
+
+    if pk:
+        get_object_or_404(ProductCategory, pk=pk)
+        product_objects = Product.objects.filter(category=pk)
+    else:
+        product_objects = Product.objects.all()
+    context = {
+        'categories': ProductCategory.objects.all(),
+        'products': product_objects,
+        'basket_quantity': total_quantity,
+    }
     return render(request, 'mainapp/products.html', context)
 
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
+
 
 def common(request):
     return render(request, 'common/index.html')
